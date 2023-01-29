e324d445a653530ffb33292239dd797aa5966ddb
SunShibo <2990452909@qq.com>
2023-01-29 22:00:42 +0800

    Added buffer layout descriptors for vertex buffers.

--- Algernon/src/Algernon/Application.cpp
+++ Algernon/src/Algernon/Application.cpp
@@ -11,6 +11,27 @@ namespace Algernon {
 
 	Application* Application::s_Instance = nullptr;
 
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+	{
+		switch (type)
+		{
+			case Algernon::ShaderDataType::Float:    return GL_FLOAT;
+			case Algernon::ShaderDataType::Float2:   return GL_FLOAT;
+			case Algernon::ShaderDataType::Float3:   return GL_FLOAT;
+			case Algernon::ShaderDataType::Float4:   return GL_FLOAT;
+			case Algernon::ShaderDataType::Mat3:     return GL_FLOAT;
+			case Algernon::ShaderDataType::Mat4:     return GL_FLOAT;
+			case Algernon::ShaderDataType::Int:      return GL_INT;
+			case Algernon::ShaderDataType::Int2:     return GL_INT;
+			case Algernon::ShaderDataType::Int3:     return GL_INT;
+			case Algernon::ShaderDataType::Int4:     return GL_INT;
+			case Algernon::ShaderDataType::Bool:     return GL_BOOL;
+		}
+
+		AL_CORE_ASSERT(false, "Unknown ShaderDataType!");
+		return 0;
+	}
+
 
 	Application::Application()
 	{
@@ -26,16 +47,36 @@ namespace Algernon {
 		glGenVertexArrays(1, &m_VertexArray);
 		glBindVertexArray(m_VertexArray);
 
-		float vertices[3 * 3] = {
-			-0.5f, -0.5f, 0.0f,
-			 0.5f, -0.5f, 0.0f,
-			 0.0f,  0.5f, 0.0f
+		float vertices[3 * 7] = {
+			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+			 0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+			 0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
 		};
 
 		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
 
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+		{
+			BufferLayout layout = {
+				{ ShaderDataType::Float3, "a_Position" },
+				{ ShaderDataType::Float4, "a_Color" }
+			};
+
+			m_VertexBuffer->SetLayout(layout);
+		}
+
+		uint32_t index = 0;
+		const auto& layout = m_VertexBuffer->GetLayout();
+		for (const auto& element : layout)
+		{
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index,
+				element.GetComponentCount(),
+				ShaderDataTypeToOpenGLBaseType(element.Type),
+				element.Normalized ? GL_TRUE : GL_FALSE,
+				layout.GetStride(),
+				(const void*)element.Offset);
+			index++;
+		}
 
 		unsigned int indices[3] = { 0, 1, 2 };
 		m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices)/sizeof(uint32_t)));
@@ -44,10 +85,15 @@ namespace Algernon {
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
+	
 			out vec3 v_Position;
+			out vec4 v_Color;
+
 			void main()
 			{
 				v_Position = a_Position;
+				v_Color = a_Color;
 				gl_Position = vec4(a_Position, 1.0);	
 			}
 		)";
@@ -56,10 +102,14 @@ namespace Algernon {
 			#version 330 core
 			
 			layout(location = 0) out vec4 color;
+
 			in vec3 v_Position;
+			in vec4 v_Color;
+
 			void main()
 			{
 				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color;
 			}
 		)";
 
