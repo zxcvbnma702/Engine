d416e3dba009980b15b4d502c84a8f5a4b4b59ae
zxcvbnma702 <83083486+zxcvbnma702@users.noreply.github.com>
2023-08-07 16:40:00 +0800

    add rotate batch ractangle

--- Sandbox/src/SandBox2D.cpp
+++ Sandbox/src/SandBox2D.cpp
@@ -42,14 +42,18 @@ void SandBox2D::OnUpdate(Algernon::Timestep ts)
 	
 	{
 		AL_PROFILE_SCOPE("Renderer Draw");
+
+		static float rotation = 0.0f;
+		rotation += ts * 50.0f;
+
 		Algernon::Renderer2D::BeginScene(m_CameraController.GetCamera());
 
-		//Algernon::Renderer2D::DrawRotatedQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, glm::radians(-45.0f), { 0.8f, 0.2f, 0.3f, 1.0f });
+		Algernon::Renderer2D::DrawRotatedQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, glm::radians(-45.0f), { 0.8f, 0.2f, 0.3f, 1.0f });
 		Algernon::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
 		Algernon::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
 		
-		/*Algernon::Renderer2D::DrawQuad({ -5.0f, -5.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 10.0f);
-		Algernon::Renderer2D::DrawQuad({ -0.5f, -0.5f, 0.0f }, { 1.0f, 1.0f }, m_CheckerboardTexture, 20.0f);*/
+		Algernon::Renderer2D::DrawQuad({ -5.0f, -5.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 10.0f);
+		Algernon::Renderer2D::DrawRotatedQuad({ -0.5f, -0.5f, 0.0f }, { 1.0f, 1.0f }, rotation,  m_CheckerboardTexture, 20.0f);
 
 		Algernon::Renderer2D::EndScene();
 	}
